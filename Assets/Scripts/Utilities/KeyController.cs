ab495e4dd0d536cd80b3639e9ca0f5e0d697a1be
kiran2701 <gaurdian.kiran@gmail.com>
2023-09-04 19:29:09 +0530

    Added Key Pickup Animation

--- Assets/Scripts/Utilities/KeyController.cs
+++ Assets/Scripts/Utilities/KeyController.cs
@@ -7,11 +7,25 @@ public class KeyController : MonoBehaviour
 {
     public static Action KeyPickedUp;
 
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
             KeyPickedUp.Invoke();
 
+        animator.SetTrigger("KeyCollected");
+        WaitBeforeDestroy();
+    }
+
+    private IEnumerator WaitBeforeDestroy()
+    {
+        yield return new WaitForSeconds(0.5f);
         Destroy(this.gameObject);
     }
 }
