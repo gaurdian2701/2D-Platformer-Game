a620595a1775b5b499b62af5b95a33fe90a6d833
kiran2701 <gaurdian.kiran@gmail.com>
2023-09-05 21:04:45 +0530

    Added Player death animation and scene reloading on death

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private int health;
+    private Animator animator;
+
+    public static Action PlayerDead;
+    public static Action<int> PlayerDamaged;
+
+
+    private void Start()
+    {
+        health = 3;
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            DecreaseHealth();
+
+            if (PlayerIsDead())
+                return;
+
+            StartCoroutine(IgnoreCollisions(collision));
+        }
+    }
+
+    private IEnumerator IgnoreCollisions(Collision2D collision)
+    {
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Enemy"), true);
+        yield return new WaitForSecondsRealtime(1.3f);
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Enemy"), false);
+    }
+
+    private void DecreaseHealth()
+    {
+        animator.SetTrigger("PlayerHurt");
+        health -= 1;
+
+        PlayerDamaged.Invoke(health);
+    }
+
+    private bool PlayerIsDead()
+    {
+        if (health <= 0)
+        {
+            health = 0;
+            PlayerDead.Invoke();
+
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ Assets/Scripts/Utilities/HandleHeartsUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandleHeartsUI : MonoBehaviour
+{
+    [SerializeField] private List<Image> HeartUI;
+
+    private void Start()
+    {
+        PlayerHealth.PlayerDamaged += DecreaseHearts;
+    }
+
+    private void OnDestroy()
+    {
+        PlayerHealth.PlayerDamaged -= DecreaseHearts;
+    }
+    private void DecreaseHearts(int health)
+    {
+        Image heart = HeartUI[health];
+        heart.fillAmount = 0f;
+    }
+}
